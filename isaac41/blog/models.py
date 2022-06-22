39b1bf0a15ebbeab84df445750676ceb7e9a6ec7
AKANINWOR ISAAC <35334852+isaac5191@users.noreply.github.com>
2022-06-22 02:39:46 +0100

    Update models.py

--- isaac41/blog/models.py
+++ isaac41/blog/models.py
@@ -5,6 +5,7 @@ from turtle import title
 from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.forms import CharField, DateTimeField
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
@@ -12,4 +13,5 @@ class Blogpost(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     Timestamp =  models.DateTimeField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,related_name="Blogpost")
     
